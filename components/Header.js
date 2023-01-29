b8e835ba42ccc2698b7c41f8c1510fb956e1cf55
emanuelefavero <emafaux89@gmail.com>
2023-01-29 22:33:17 +0100

    add home effects in header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+  const router = useRouter()
   return (
     <header>
       <div className='container'>
-        <h2>
-          <Link href='/'>
+        <h2 className={router.pathname === '/' ? 'home' : 'not-home'}>
+          <Link
+            href='/'
+            className={router.pathname === '/' ? 'home' : 'not-home'}
+          >
             {'<'}
             {'>'} Blog
           </Link>
